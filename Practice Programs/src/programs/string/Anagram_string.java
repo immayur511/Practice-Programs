c591c8b9bdda34c25e2a82fe8ea785fa05c9b812
Mayur <mayurmrdikar@gmail.com>
2021-09-12 12:34:08 +0530

    new class added

---
+++ Practice Programs/src/programs/string/Anagram_string.java
@@ -0,0 +1,24 @@
+package programs.string;
+
+public class Anagram_string {
+
+	public static void main(String[] args) {
+		
+		String str1="init";
+		String str2="tini";
+		
+		
+		char[] arr=str1.toCharArray();
+		char[] arr2=str2.toCharArray();
+		
+		
+		
+		System.out.println(arr);
+		if(str1.length()==str2.length()) {
+			
+			
+		}
+
+	}
+
+}
